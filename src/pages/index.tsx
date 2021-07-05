37930e15edb64addcf5efe8412ee92dce03a0ea3
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-05 15:27:22 -0300

    commit da estrutura inicial do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+}
