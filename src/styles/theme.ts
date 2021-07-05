c590c05a2b0b1a773b5cb91e456f3d843d8ae501
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-05 15:55:57 -0300

    chore: instalando e configurando o chakra-ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        background: "F5F8FA",
+      },
+    },
+  },
+});
