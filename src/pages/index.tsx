b7a7dbd86854ee6152d977cfcf87a559413394c2
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-06 20:01:20 -0300

    feat: importando o component continents e estilizando o box divider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import { Box, Stack } from "@chakra-ui/react";
+import { Box, Flex, Stack } from "@chakra-ui/react";
 import Head from "next/head";
+
 import { BannerHome } from "../components/BannerHome";
+import { Continents } from "../components/Continents";
 import { Header } from "../components/Header";
 import { TravelTypes } from "../components/TravelTypes";
+
 export default function Home() {
   return (
     <>
@@ -15,6 +18,10 @@ export default function Home() {
         <Stack spacing="6">
           <BannerHome />
           <TravelTypes />
+          <Flex align="center" justify="center">
+            <Box w="90px" h="2px" bg="#47585B" />
+          </Flex>
+          <Continents />
         </Stack>
       </Box>
     </>
