7c3d4200b39e5a31e52682f4f00d938aeae8675d
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-07 08:55:00 -0300

    feat: separando o swiper do component e ajustando o responsívo do component

--- src/components/Continents.tsx
+++ src/components/Continents.tsx
@@ -7,8 +7,48 @@ import "swiper/components/navigation/navigation.min.css";
 
 import SwiperCore, { Pagination, Navigation } from "swiper/core";
 import React from "react";
+import { ConstinentsSlide } from "./ContinentsSlide";
 
 SwiperCore.use([Pagination, Navigation]);
+
+const continents = [
+  {
+    id: 1,
+    name: "Europa",
+    description: "O continente mais antigo.",
+    bg_url: "./continents/europe.png",
+  },
+  {
+    id: 1,
+    name: "Asia",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/asia.png",
+  },
+  {
+    id: 1,
+    name: "América do sul",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/south_america.png",
+  },
+  {
+    id: 1,
+    name: "América do norte",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/north_america.png",
+  },
+  {
+    id: 1,
+    name: "Africa",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/africa.png",
+  },
+  {
+    id: 1,
+    name: "oceania",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/oceania.png",
+  },
+];
 
 export function Continents() {
   return (
@@ -21,175 +61,15 @@ export function Continents() {
         paddingY="6"
       >
         <Stack align="center">
-          <Text fontSize="36" fontWeight="500">
+          <Text fontSize={["20", "36"]} fontWeight="500">
             Vamos nessa?
           </Text>
-          <Text fontSize="36" fontWeight="500">
+          <Text fontSize={["20", "36"]} fontWeight="500">
             Então escolha seu continente
           </Text>
         </Stack>
         <Box w="100%" minH="450px" paddingY="6">
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={16}
-            loop={true}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            className="swiper-container"
-          >
-            <SwiperSlide>
-              <Box
-                w="100%"
-                h="100%"
-                background="url('./continents/europe.png') center center no-repeat"
-                backgroundSize="cover"
-              >
-                <Flex
-                  w="100%"
-                  h="100%"
-                  background="rgba(0,0,0,0.4)"
-                  align="center"
-                  justify="center"
-                >
-                  <Stack spacing="2">
-                    <Text fontSize="48" fontWeight="700" color="#fff">
-                      Europa
-                    </Text>
-                    <Text fontSize="24" fontWeight="700" color="#DADADA">
-                      O continente mais antigo.
-                    </Text>
-                  </Stack>
-                </Flex>
-              </Box>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Box
-                w="100%"
-                h="100%"
-                background="url('./continents/asia.png') center center no-repeat"
-                backgroundSize="cover"
-              >
-                <Flex
-                  w="100%"
-                  h="100%"
-                  background="rgba(0,0,0,0.4)"
-                  align="center"
-                  justify="center"
-                >
-                  <Stack spacing="2">
-                    <Text fontSize="48" fontWeight="700" color="#fff">
-                      Asia
-                    </Text>
-                    <Text fontSize="24" fontWeight="700" color="#DADADA">
-                      Lorem ipsum dolor sit amet.
-                    </Text>
-                  </Stack>
-                </Flex>
-              </Box>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Box
-                w="100%"
-                h="100%"
-                background="url('./continents/africa.png') center center no-repeat"
-                backgroundSize="cover"
-              >
-                <Flex
-                  w="100%"
-                  h="100%"
-                  background="rgba(0,0,0,0.4)"
-                  align="center"
-                  justify="center"
-                >
-                  <Stack spacing="2">
-                    <Text fontSize="48" fontWeight="700" color="#fff">
-                      Africa
-                    </Text>
-                    <Text fontSize="24" fontWeight="700" color="#DADADA">
-                      Lorem ipsum dolor sit amet.
-                    </Text>
-                  </Stack>
-                </Flex>
-              </Box>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Box
-                w="100%"
-                h="100%"
-                background="url('./continents/oceania.png') center center no-repeat"
-                backgroundSize="cover"
-              >
-                <Flex
-                  w="100%"
-                  h="100%"
-                  background="rgba(0,0,0,0.4)"
-                  align="center"
-                  justify="center"
-                >
-                  <Stack spacing="2">
-                    <Text fontSize="48" fontWeight="700" color="#fff">
-                      Oceania
-                    </Text>
-                    <Text fontSize="24" fontWeight="700" color="#DADADA">
-                      Lorem ipsum dolor sit amet.
-                    </Text>
-                  </Stack>
-                </Flex>
-              </Box>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Box
-                w="100%"
-                h="100%"
-                background="url('./continents/south_america.png') center center no-repeat"
-                backgroundSize="cover"
-              >
-                <Flex
-                  w="100%"
-                  h="100%"
-                  background="rgba(0,0,0,0.4)"
-                  align="center"
-                  justify="center"
-                >
-                  <Stack spacing="2">
-                    <Text fontSize="48" fontWeight="700" color="#fff">
-                      América do sul
-                    </Text>
-                    <Text fontSize="24" fontWeight="700" color="#DADADA">
-                      Lorem ipsum dolor sit amet.
-                    </Text>
-                  </Stack>
-                </Flex>
-              </Box>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Box
-                w="100%"
-                h="100%"
-                background="url('./continents/north_america.png') center center no-repeat"
-                backgroundSize="cover"
-              >
-                <Flex
-                  w="100%"
-                  h="100%"
-                  background="rgba(0,0,0,0.4)"
-                  align="center"
-                  justify="center"
-                >
-                  <Stack spacing="2">
-                    <Text fontSize="48" fontWeight="700" color="#fff">
-                      América do norte
-                    </Text>
-                    <Text fontSize="24" fontWeight="700" color="#DADADA">
-                      Lorem ipsum dolor sit amet.
-                    </Text>
-                  </Stack>
-                </Flex>
-              </Box>
-            </SwiperSlide>
-          </Swiper>
+          <ConstinentsSlide continents={continents} />
         </Box>
       </Flex>
     </Box>
---
+++ src/components/ContinentsSlide.tsx
@@ -0,0 +1,77 @@
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
+
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
+import React from "react";
+
+SwiperCore.use([Pagination, Navigation]);
+
+type Continents = {
+  id: number;
+  name: string;
+  description: string;
+  bg_url: string;
+};
+
+interface ConstinentsSlideProps {
+  continents: Continents[];
+}
+
+export function ConstinentsSlide({ continents }: ConstinentsSlideProps) {
+  return (
+    <>
+      {continents && (
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={16}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          className="swiper-container"
+        >
+          {continents.map((item) => (
+            <SwiperSlide key={item.name}>
+              <Box
+                w="100%"
+                h="100%"
+                background={`url(${item.bg_url}) center center no-repeat`}
+                backgroundSize="cover"
+              >
+                <Flex
+                  w="100%"
+                  h="100%"
+                  background="rgba(0,0,0,0.4)"
+                  align="center"
+                  justify="center"
+                >
+                  <Stack spacing="2">
+                    <Text
+                      fontSize={["24", "36", "48"]}
+                      fontWeight="700"
+                      color="#fff"
+                    >
+                      {item.name}
+                    </Text>
+                    <Text
+                      fontSize={["14", "18", "24"]}
+                      fontWeight="700"
+                      color="#DADADA"
+                    >
+                      {item.description}
+                    </Text>
+                  </Stack>
+                </Flex>
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </>
+  );
+}
