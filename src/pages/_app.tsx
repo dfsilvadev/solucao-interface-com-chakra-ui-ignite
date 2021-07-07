ab78f44b59dbbdfd2aa10432233e26937fb32888
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-07 14:12:37 -0300

    feat: criando um server com miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,56 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { createServer } from "miragejs";
 
 import { theme } from "../styles/theme";
 import "../styles/global.scss";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/continents", () => {
+      return [
+        {
+          id: 1,
+          name: "Europa",
+          description: "O continente mais antigo.",
+          bg_url: "./continents/europe.png",
+        },
+        {
+          id: 1,
+          name: "Asia",
+          description: "Lorem Ipsum",
+          bg_url: "./continents/asia.png",
+        },
+        {
+          id: 1,
+          name: "América do sul",
+          description: "Lorem Ipsum",
+          bg_url: "./continents/south_america.png",
+        },
+        {
+          id: 1,
+          name: "América do norte",
+          description: "Lorem Ipsum",
+          bg_url: "./continents/north_america.png",
+        },
+        {
+          id: 1,
+          name: "Africa",
+          description: "Lorem Ipsum",
+          bg_url: "./continents/africa.png",
+        },
+        {
+          id: 1,
+          name: "oceania",
+          description: "Lorem Ipsum",
+          bg_url: "./continents/oceania.png",
+        },
+      ];
+    });
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
