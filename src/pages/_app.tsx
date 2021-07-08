73dc720fc07756d1b48f857908c5af56ce5cbfce
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-08 13:31:01 -0300

    feat: importando o context e estartando o service do mirage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,61 +1,23 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { createServer } from "miragejs";
+
+import { ContinentsAndCountriesProvider } from "../contexts/ContinentsAndCountriesContext";
+import { makeServer } from "../services/mirage";
 
 import { theme } from "../styles/theme";
 import "../styles/global.scss";
 
-createServer({
-  routes() {
-    this.namespace = "api";
-    this.get("/continents", () => {
-      return [
-        {
-          id: 1,
-          name: "Europa",
-          description: "O continente mais antigo.",
-          bg_url: "./continents/europe.png",
-        },
-        {
-          id: 1,
-          name: "Asia",
-          description: "Lorem Ipsum",
-          bg_url: "./continents/asia.png",
-        },
-        {
-          id: 1,
-          name: "América do sul",
-          description: "Lorem Ipsum",
-          bg_url: "./continents/south_america.png",
-        },
-        {
-          id: 1,
-          name: "América do norte",
-          description: "Lorem Ipsum",
-          bg_url: "./continents/north_america.png",
-        },
-        {
-          id: 1,
-          name: "Africa",
-          description: "Lorem Ipsum",
-          bg_url: "./continents/africa.png",
-        },
-        {
-          id: 1,
-          name: "oceania",
-          description: "Lorem Ipsum",
-          bg_url: "./continents/oceania.png",
-        },
-      ];
-    });
-  },
-});
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <ContinentsAndCountriesProvider>
+        <Component {...pageProps} />
+      </ContinentsAndCountriesProvider>
     </ChakraProvider>
   );
 }
