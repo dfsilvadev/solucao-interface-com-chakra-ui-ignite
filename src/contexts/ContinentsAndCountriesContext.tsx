6d97f320f8816555e512344310b566264a3f95d6
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-09 18:57:48 -0300

    feat: criando contexto para acesso as informações do app

--- src/contexts/ContinentsAndCountriesContext.tsx
+++ src/contexts/ContinentsAndCountriesContext.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import { createContext } from "react";
+import { api } from "../services/api";
 
-const ContinentsAndCountriesContext = createContext([]);
+const ContinentsAndCountriesContext = createContext({});
+
+type Continents = {
+  id: number;
+  name: string;
+  description: string;
+  bg_url: string;
+};
 
 export function ContinentsAndCountriesProvider({ children }) {
+  const [continents, setContinents] = useState<Continents[]>([]);
+
+  useEffect(() => {
+    api.get("continents").then((response) => setContinents(response.data));
+  }, []);
+
   return (
-    <ContinentsAndCountriesContext.Provider value={[]}>
+    <ContinentsAndCountriesContext.Provider value={{ continents }}>
       {children}
     </ContinentsAndCountriesContext.Provider>
   );
