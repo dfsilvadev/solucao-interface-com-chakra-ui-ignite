1722126dbc0da2193202d95cdfa0049b45073cb8
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-09 18:58:38 -0300

    feat: dados dos continentes

---
+++ src/utils/dataContinents.ts
@@ -0,0 +1,38 @@
+export const dataContinents = [
+  {
+    id: 1,
+    name: "Europa",
+    description: "O continente mais antigo.",
+    bg_url: "./continents/europe.png",
+  },
+  {
+    id: 2,
+    name: "Asia",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/asia.png",
+  },
+  {
+    id: 3,
+    name: "América do sul",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/south_america.png",
+  },
+  {
+    id: 4,
+    name: "América do norte",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/north_america.png",
+  },
+  {
+    id: 5,
+    name: "Africa",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/africa.png",
+  },
+  {
+    id: 6,
+    name: "oceania",
+    description: "Lorem Ipsum",
+    bg_url: "./continents/oceania.png",
+  },
+];
