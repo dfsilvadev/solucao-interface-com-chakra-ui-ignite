be23c8a9a8dcfa9cf7b1dcc9044c02e85d5b1748
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-12 19:32:52 -0300

    feat: criando os dados fake de países1

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,23 +1,44 @@
 import { createServer, Model } from "miragejs";
 
-import { dataContinents } from "../../utils/dataContinents";
+import {
+  dataContinents,
+  dataCountriesByContinent,
+} from "../../utils/dataUtils";
 
-type Continents = {
+type Continent = {
   id: number;
   name: string;
   description: string;
   bg_url: string;
 };
 
+type Country = {
+  id: number;
+  name: string;
+  capital: string;
+  capa?: string;
+  flag?: string;
+};
+
+type CountryByContinent = {
+  id: number;
+  name: string;
+  description: string;
+  bg_url: string;
+  countries: Country[];
+};
+
 export function makeServer() {
   const server = createServer({
     models: {
-      continent: Model.extend<Continents>({} as Continents),
+      continent: Model.extend<Continent>({} as Continent),
+      country: Model.extend<CountryByContinent>({} as CountryByContinent),
     },
 
     routes() {
       this.namespace = "api";
       this.get("/continents", () => dataContinents);
+      this.get("/countries", () => dataCountriesByContinent);
       this.namespace = "";
       this.passthrough();
     },
