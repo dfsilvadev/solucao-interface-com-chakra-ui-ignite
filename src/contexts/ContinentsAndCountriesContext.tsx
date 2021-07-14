ec192fbdaba4f7e8d21d46c87ad5e51aba6b8cdb
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-14 10:00:21 -0300

    feat: buscando os dados dos países

--- src/contexts/ContinentsAndCountriesContext.tsx
+++ src/contexts/ContinentsAndCountriesContext.tsx
@@ -9,36 +9,53 @@ interface Continents {
   bg_url: string;
 }
 
+type Country = {
+  id: number;
+  name: string;
+  capital: string;
+  capa?: string;
+  flag?: string;
+};
+
+type CountryByContinent = {
+  id: number;
+  name: string;
+  description: string;
+  bg_url: string;
+  countries: Country[];
+};
+
 interface ContinentsAndCountriesProviderProps {
   children: ReactNode;
 }
 
 interface ContinentsAndCountriesData {
   continents: Continents[];
-  getCountries;
+  countries: CountryByContinent[];
 }
 
-const ContinentsAndCountriesContext = createContext({});
+const ContinentsAndCountriesContext = createContext(
+  {} as ContinentsAndCountriesData
+);
 
 export function ContinentsAndCountriesProvider({
   children,
 }: ContinentsAndCountriesProviderProps) {
   const [continents, setContinents] = useState<Continents[]>([]);
+  const [countries, setCountries] = useState<CountryByContinent[]>([]);
 
-  async function getCountries(continent: string) {
-    const response = await api
-      .get("countries")
-      .then((response) => response.data);
+  useEffect(() => {
+    api.get("countries").then((response) => setCountries(response.data));
+  }, []);
 
-    console.log(response);
-  }
+  async function getCountries() {}
 
   useEffect(() => {
     api.get("continents").then((response) => setContinents(response.data));
   }, []);
 
   return (
-    <ContinentsAndCountriesContext.Provider value={{ continents }}>
+    <ContinentsAndCountriesContext.Provider value={{ continents, countries }}>
       {children}
     </ContinentsAndCountriesContext.Provider>
   );
