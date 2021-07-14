abae2538f1c26d3afb989d5adcb61ffde20a98e4
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-14 10:00:43 -0300

    feat: alterando a url das imagens

--- src/utils/dataUtils.ts
+++ src/utils/dataUtils.ts
@@ -37,13 +37,13 @@ export const dataContinents = [
   },
 ];
 
-export const dataCountries = [
+export const dataCountriesByContinent = [
   {
     id: 1,
     name: "Europa",
     description:
       "A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste",
-    bg_url: "./continents/europe.png",
+    bg_url: "./countries/europe.png",
     countries: [
       {
         id: 1,
